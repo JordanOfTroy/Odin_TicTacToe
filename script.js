15ce4afd2f65d440fff953a08a8c4f9cc37ae38b
Jordan Smithson <Jordan Smithson>
2021-02-25 19:09:08 -0700

    added basic game board

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 // You got this!
+/**You’re going to store the gameboard as an array inside of a Gameboard object, 
+ * so start there! Your players are also going to be stored in objects… and you’re 
+ * probably going to want an object to control the flow of the game itself. */
+
+
+const gameBoard = (() => {
+    const _board = {
+        spaces: new Array(9)
+    };
+    return {
+        _board
+    }
+})()
+console.log('testing:', gameBoard)
+
+
+const player = (name, marker) => {
+
+
+}
+
